d56a46652d8917f0fd0b323fe512c8027a372124
Andrea Horbach <andrea.horbach@uni-duisburg-essen.de>
2018-04-19 22:56:45 +0200

    add readability measures #30

---
+++ de.unidue.ltl.escrito/de.unidue.ltl.escrito.features/src/main/java/de/unidue/ltl/escrito/features/complexity/TraditionalReadabilityMeasures.java
@@ -0,0 +1,118 @@
+package de.unidue.ltl.escrito.features.complexity;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.uima.fit.descriptor.ConfigurationParameter;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+import de.tudarmstadt.ukp.dkpro.core.readability.measure.ReadabilityMeasures;
+import de.tudarmstadt.ukp.dkpro.core.readability.measure.ReadabilityMeasures.Measures;
+import org.dkpro.tc.api.exception.TextClassificationException;
+import org.dkpro.tc.api.features.Feature;
+import org.dkpro.tc.api.features.FeatureExtractor;
+import org.dkpro.tc.api.features.FeatureExtractorResource_ImplBase;
+import org.dkpro.tc.api.features.FeatureType;
+import org.dkpro.tc.api.type.TextClassificationTarget;
+
+public class TraditionalReadabilityMeasures
+    extends FeatureExtractorResource_ImplBase
+    implements FeatureExtractor
+{
+    /**
+     * Computes the readability measures ari, coleman_liau, flesch, fog, kincaid, lix and smog as
+     * implemented in de.tudarmstadt.ukp.dkpro.core.readability-asl s
+     */
+    // having all these parameters is not nice
+    // better: several instances of extractor with measure as resource
+    public static final String PARAM_ADD_KINCAID = "kincaid";
+    @ConfigurationParameter(name = PARAM_ADD_KINCAID, mandatory = false, defaultValue = "true")
+    protected boolean kincaid;
+
+    public static final String PARAM_ADD_ARI = "ari";
+    @ConfigurationParameter(name = PARAM_ADD_ARI, mandatory = false)
+    protected boolean ari;
+
+    public static final String PARAM_ADD_COLEMANLIAU = "coleman_liau";
+    @ConfigurationParameter(name = PARAM_ADD_ARI, mandatory = true, defaultValue = "true")
+    protected boolean colemanLiau;
+
+    public static final String PARAM_ADD_FLESH = "flesch";
+    @ConfigurationParameter(name = PARAM_ADD_FLESH, mandatory = false)
+    protected boolean flesh;
+
+    public static final String PARAM_ADD_FOG = "fog";
+    @ConfigurationParameter(name = PARAM_ADD_FOG, mandatory = false)
+    protected boolean fog;
+
+    public static final String PARAM_ADD_LIX = "lix";
+    @ConfigurationParameter(name = PARAM_ADD_LIX, mandatory = false)
+    protected boolean lix;
+
+    public static final String PARAM_ADD_SMOG = "smog";
+    @ConfigurationParameter(name = PARAM_ADD_ARI, mandatory = false)
+    protected boolean smog;
+
+    @Override
+    public Set<Feature> extract(JCas jcas, TextClassificationTarget target)
+        throws TextClassificationException
+    {
+
+        ReadabilityMeasures readability = new ReadabilityMeasures();
+        Set<Feature> featSet = new HashSet<Feature>();
+        if (jcas.getDocumentLanguage() != null) {
+            readability.setLanguage(jcas.getDocumentLanguage());
+        }
+
+        int nrOfSentences = JCasUtil.select(jcas, Sentence.class).size();
+        List<String> words = new ArrayList<String>();
+        for (Token t : JCasUtil.select(jcas, Token.class)) {
+            words.add(t.getCoveredText());
+        }
+
+        // only add features for selected readability measures
+        Measures measure;
+        if (ari) {
+            measure = Measures.valueOf(PARAM_ADD_ARI);
+            featSet.add(new Feature(PARAM_ADD_ARI, readability.getReadabilityScore(measure, words,
+                    nrOfSentences), FeatureType.NUMERIC));
+        }
+        if (kincaid) {
+            measure = Measures.valueOf(PARAM_ADD_KINCAID);
+            featSet.add(new Feature(PARAM_ADD_KINCAID, readability.getReadabilityScore(measure,
+                    words, nrOfSentences), FeatureType.NUMERIC));
+        }
+        if (colemanLiau) {
+            measure = Measures.valueOf(PARAM_ADD_COLEMANLIAU);
+            featSet.add(new Feature(PARAM_ADD_COLEMANLIAU, readability.getReadabilityScore(
+                    measure, words, nrOfSentences), FeatureType.NUMERIC));
+        }
+        if (flesh) {
+            measure = Measures.valueOf(PARAM_ADD_FLESH);
+            featSet.add(new Feature(PARAM_ADD_FLESH, readability.getReadabilityScore(measure,
+                    words, nrOfSentences), FeatureType.NUMERIC));
+        }
+        if (fog) {
+            measure = Measures.valueOf(PARAM_ADD_FOG);
+            featSet.add(new Feature(PARAM_ADD_FOG, readability.getReadabilityScore(measure, words,
+                    nrOfSentences), FeatureType.NUMERIC));
+        }
+        if (smog) {
+            measure = Measures.valueOf(PARAM_ADD_SMOG);
+            featSet.add(new Feature(PARAM_ADD_SMOG, readability.getReadabilityScore(measure,
+                    words, nrOfSentences), FeatureType.NUMERIC));
+        }
+        if (lix) {
+            measure = Measures.valueOf(PARAM_ADD_LIX);
+            featSet.add(new Feature(PARAM_ADD_LIX, readability.getReadabilityScore(measure, words,
+                    nrOfSentences), FeatureType.NUMERIC));
+        }
+        return featSet;
+    }
+
+}
---
+++ de.unidue.ltl.escrito/de.unidue.ltl.escrito.features/src/test/java/de/unidue/ltl/escrito/features/complexity/TraditionalReadabilityMeasuresTest.java
@@ -0,0 +1,65 @@
+package de.unidue.ltl.escrito.features.complexity;
+
+import static org.dkpro.tc.testing.FeatureTestUtil.assertFeature;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngineDescription;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.jcas.JCas;
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
+import de.unidue.ltl.escrito.features.coherencecohesion.NrOfConnectives;
+
+import org.dkpro.tc.api.features.Feature;
+import org.dkpro.tc.api.features.util.FeatureUtil;
+import org.dkpro.tc.api.type.TextClassificationTarget;
+
+public class TraditionalReadabilityMeasuresTest
+{
+
+    @Test
+    public void readabilityFeatureExtractorTest()
+        throws Exception
+    {
+        // HashMap<String, Double> correctResult = new HashMap<String, Double>();
+        // correctResult.put("kincaid", 7.6);
+        // correctResult.put("ari", 9.1);
+        // correctResult.put("coleman_liau", 11.6);
+        // correctResult.put("flesch", 70.6);
+        // correctResult.put("lix", 5.0);
+        // correctResult.put("smog", 9.9);
+        // correctResult.put("fog", 10.6);
+
+        String testDocument = FileUtils.readFileToString(new File("src/test/resources/test_document_en.txt"));
+
+        AnalysisEngineDescription desc = createEngineDescription(BreakIteratorSegmenter.class);
+        AnalysisEngine engine = createEngine(desc);
+
+        JCas jcas = engine.newJCas();
+        jcas.setDocumentLanguage("en");
+        jcas.setDocumentText(testDocument);
+        engine.process(jcas);
+
+        TraditionalReadabilityMeasures extractor = FeatureUtil.createResource(
+        		TraditionalReadabilityMeasures.class,
+        		TraditionalReadabilityMeasures.PARAM_ADD_COLEMANLIAU, "true",
+        		TraditionalReadabilityMeasures.PARAM_UNIQUE_EXTRACTOR_NAME, "dummy");
+
+        List<Feature> features = new ArrayList<>(extractor.extract(jcas, TextClassificationTarget.get(jcas)));
+
+        Assert.assertEquals(2, features.size());
+        Iterator<Feature> iter = features.iterator();
+        assertFeature(TraditionalReadabilityMeasures.PARAM_ADD_KINCAID, 7.6, iter.next(), 0.1);
+        assertFeature(TraditionalReadabilityMeasures.PARAM_ADD_COLEMANLIAU, 11.6, iter.next(), 0.1);
+
+    }
+}
