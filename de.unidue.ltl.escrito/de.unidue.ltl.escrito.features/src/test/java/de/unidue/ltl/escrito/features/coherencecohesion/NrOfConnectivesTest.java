a3fef06b63ab589c62e892bdea55b46a7b179eb4
Andrea Horbach <andrea.horbach@uni-duisburg-essen.de>
2018-04-19 16:47:58 +0200

    fix

--- de.unidue.ltl.escrito/de.unidue.ltl.escrito.features/src/test/java/de/unidue/ltl/escrito/features/coherencecohesion/NrOfConnectivesTest.java
+++ de.unidue.ltl.escrito/de.unidue.ltl.escrito.features/src/test/java/de/unidue/ltl/escrito/features/coherencecohesion/NrOfConnectivesTest.java
@@ -21,8 +21,8 @@ public class NrOfConnectivesTest
 	extends EssayGradingTestBase
 {
 	@Test
-	public void nrOfConnectivesFeatureExtractorTest() throws Exception {
-		AnalysisEngine engine = getPreprocessingEngine();
+	public void nrOfConnectivesFeatureExtractorTest_de() throws Exception {
+		AnalysisEngine engine = getPreprocessingEngine("de");
 
 		JCas jcas = engine.newJCas();
 		jcas.setDocumentLanguage("de");
@@ -43,6 +43,31 @@ public class NrOfConnectivesTest
 		// 3 connectives/ 18 tokens= 0.16666666666666666 
 		Feature f = iter.next();
 		assertFeature(NrOfConnectives.NR_OF_CONNECTIVES, 0.16666666666666666, f);
+		
+	}
+	
+	
+	@Test
+	public void nrOfConnectivesFeatureExtractorTest_en() throws Exception {
+		AnalysisEngine engine = getPreprocessingEngine("en");
+
+		JCas jcas = engine.newJCas();
+		jcas.setDocumentLanguage("en");
+		// Prepositions trotz nach zur während vor
+		jcas.setDocumentText("Because I love connectives since I learned about them.");
+		engine.process(jcas);
+
+		NrOfConnectives extractor = FeatureUtil.createResource(
+				NrOfConnectives.class,
+				NrOfConnectives.PARAM_CONNECTIVES_FILE_PATH, "src/main/resources/lists/en/connectives_en.txt"
+		);
+        Set<Feature> features = extractor.extract(jcas, TextClassificationTarget.get(jcas));
+
+		Assert.assertEquals(1, features.size());
+
+		Iterator<Feature> iter = features.iterator();
+		// 2 connectives/ 10 tokens= 0.2 
+		assertFeature(NrOfConnectives.NR_OF_CONNECTIVES, 0.2, iter.next());
 		
 	}
 }
