15ff879cd508f4c75285e087a59343885ad5599d
Andrea Horbach <andrea.horbach@uni-duisburg-essen.de>
2018-05-01 18:53:58 +0200

    Add latex output for LC report

--- de.unidue.ltl.escrito/de.unidue.ltl.escrito.core/src/main/java/de/unidue/ltl/escrito/core/learningcurve/LearningCurveReport.java
+++ de.unidue.ltl.escrito/de.unidue.ltl.escrito.core/src/main/java/de/unidue/ltl/escrito/core/learningcurve/LearningCurveReport.java
@@ -24,6 +24,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -182,11 +183,11 @@ implements Constants
 			minValues.add(min);
 			maxValues.add(max);
 			avgValues.add(meanKappa);
-			
+
 			for (String s : results.keySet()) {
 				props.setProperty(s, results.get(s).toString());
 			}
-			
+
 			// Write out properties
 			getContext().getStorageService().storeBinary(LearningCurveTaskId, RESULTS_FILENAME + "_" + numberOfInstances+".txt", new PropertiesAdapter(props));
 			//	getContext().storeBinary(RESULTS_FILENAME + "_" + numberOfInstances+".txt", new PropertiesAdapter(props));
@@ -210,6 +211,54 @@ implements Constants
 		plotter.addSeries(numInstances, maxValues, "max", Color.GREEN);
 		plotter.addSeries(numInstances, avgValues, "mean", Color.BLACK);
 		plotter.plot(getContext().getStorageService().locateKey(LearningCurveTaskId, "learningCurve.jpeg"));
+		writeLatex(numInstances, minValues, maxValues, avgValues, LearningCurveTaskId);
+	}
+
+	private void writeLatex(List<Double> numInstances, List<Double> minValues, List<Double> maxValues,
+			List<Double> avgValues, String taskId) {
+		try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(getContext().getStorageService().locateKey(taskId, "learningCurveResult.tex")));
+			bw.write("\\begin{tikzpicture}\n"
+					+ "		\\node [draw=none] at (1,3) {2.3};\n"
+					+ "\\begin{semilogxaxis}[\n"
+					+ "ylabel = {QWK},\n"
+					+ "xlabel={\\# Trainingsdaten},\n"
+					+ "height=5cm,\n"
+					+ "width=6cm,\n"
+					+ "ymin=0.0,\n"
+					+ "ymax= 1.0,\n"
+					+ "	    log basis x=2,\n"
+					+ "		xtick={4,8,16,32,64,128},\n"
+					+ "		ytick={0.0,0.1,...,1.1},legend style={at={(1.1,0.5)},anchor=west}\n"
+					+ "		]\n"
+					+ "		\\addplot[solid, color=red, thick] table[x=AmountTraining, y=Kappa] {AmountTraining Kappa\n");
+			for (int i=0; i<numInstances.size(); i++){
+				bw.write(numInstances.get(i)+" "+minValues.get(i)+"\n");
+			}
+			bw.write("};\n"
+					+ "	\\addlegendentry{worst}\\legend{}\n"
+					+ "	\\addplot[solid, color=green, thick] table[x=AmountTraining, y=Kappa] {AmountTraining Kappa\n");
+			for (int i=0; i<numInstances.size(); i++){
+				bw.write(numInstances.get(i)+" "+maxValues.get(i)+"\n");
+			}
+			bw.write("	};\n"
+					+ "	\\addlegendentry{best}\\legend{}\n"
+					+ "		\\addplot[solid, color=black, thick] table[x=AmountTraining, y=Kappa] {AmountTraining Kappa\n");
+			for (int i=0; i<numInstances.size(); i++){
+				bw.write(numInstances.get(i)+" "+avgValues.get(i)+"\n");
+			}	
+			bw.write("	};\n"
+					+ "\\addlegendentry{average}\\legend{}\n"
+					+ "	\\end{semilogxaxis}\n"
+					+ "\\end{tikzpicture}\n");
+			bw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+
+
 	}
 }
 
