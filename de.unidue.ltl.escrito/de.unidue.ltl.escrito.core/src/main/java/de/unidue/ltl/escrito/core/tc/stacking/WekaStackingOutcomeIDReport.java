0fe290086d419d71f3eff70d50cfd2626173f34a
Andrea Horbach <andrea.horbach@uni-duisburg-essen.de>
2019-02-14 15:43:59 +0100

    comment out stacking report

--- de.unidue.ltl.escrito/de.unidue.ltl.escrito.core/src/main/java/de/unidue/ltl/escrito/core/tc/stacking/WekaStackingOutcomeIDReport.java
+++ de.unidue.ltl.escrito/de.unidue.ltl.escrito.core/src/main/java/de/unidue/ltl/escrito/core/tc/stacking/WekaStackingOutcomeIDReport.java
@@ -15,53 +15,53 @@ import weka.core.Instances;
 
 public class WekaStackingOutcomeIDReport extends WekaOutcomeIDReport{
 
-	
-	 private File mlResults;
-	
-	  @Override
-	    public void execute()
-	        throws Exception
-	    {
-	        File arff = WekaUtils.getFile(getContext(), "",
-	                AdapterNameEntries.predictionsFile, AccessMode.READONLY);
-	        mlResults = WekaUtils.getFile(getContext(), "",
-	                WekaStackingTask.evaluationBin, AccessMode.READONLY);
-
-	        boolean multiLabel = getDiscriminators()
-	                .get(WekaStackingTask.class.getName() + "|" + Constants.DIM_LEARNING_MODE)
-	                .equals(Constants.LM_MULTI_LABEL);
-	        boolean regression = getDiscriminators()
-	                .get(WekaStackingTask.class.getName() + "|" + Constants.DIM_LEARNING_MODE)
-	                .equals(Constants.LM_REGRESSION);
-
-	        Instances predictions = WekaUtils.getInstances(arff, multiLabel);
-
-	        List<String> labels = getLabels(predictions, multiLabel, regression);
-	        
-	        Properties props;
-	        
-	        if(multiLabel){
-	        	MultilabelResult r = WekaUtils.readMlResultFromFile(mlResults);
-	        	props = generateMlProperties(predictions, labels, r);
-	        }
-	        else{
-	        	props = generateSlProperties(predictions, regression, labels);
-	        }
-	        
-
-	        
-	        getContext().storeBinary(Constants.ID_OUTCOME_KEY,
-	                new PropertiesAdapter(props, generateHeader(labels)));
-	    }
-	  
-	
-	  private List<String> getLabels(Instances predictions, boolean multiLabel, boolean regression)
-	    {
-	        if (regression) {
-	            return Collections.emptyList();
-	        }
-
-	        return WekaUtils.getClassLabels(predictions, multiLabel);
-	    }
+//	
+//	 private File mlResults;
+//	
+//	  @Override
+//	    public void execute()
+//	        throws Exception
+//	    {
+//	        File arff = WekaUtils.getFile(getContext(), "",
+//	                AdapterNameEntries.predictionsFile, AccessMode.READONLY);
+//	        mlResults = WekaUtils.getFile(getContext(), "",
+//	                WekaStackingTask.evaluationBin, AccessMode.READONLY);
+//
+//	        boolean multiLabel = getDiscriminators()
+//	                .get(WekaStackingTask.class.getName() + "|" + Constants.DIM_LEARNING_MODE)
+//	                .equals(Constants.LM_MULTI_LABEL);
+//	        boolean regression = getDiscriminators()
+//	                .get(WekaStackingTask.class.getName() + "|" + Constants.DIM_LEARNING_MODE)
+//	                .equals(Constants.LM_REGRESSION);
+//
+//	        Instances predictions = WekaUtils.getInstances(arff, multiLabel);
+//
+//	        List<String> labels = getLabels(predictions, multiLabel, regression);
+//	        
+//	        Properties props;
+//	        
+//	        if(multiLabel){
+//	        	MultilabelResult r = WekaUtils.readMlResultFromFile(mlResults);
+//	        	props = generateMlProperties(predictions, labels, r);
+//	        }
+//	        else{
+//	        	props = generateSlProperties(predictions, regression, labels);
+//	        }
+//	        
+//
+//	        
+//	        getContext().storeBinary(Constants.ID_OUTCOME_KEY,
+//	                new PropertiesAdapter(props, generateHeader(labels)));
+//	    }
+//	  
+//	
+//	  private List<String> getLabels(Instances predictions, boolean multiLabel, boolean regression)
+//	    {
+//	        if (regression) {
+//	            return Collections.emptyList();
+//	        }
+//
+//	        return WekaUtils.getClassLabels(predictions, multiLabel);
+//	    }
 	
 }
