6f10e3283afb4ea49a7127c4104e0ede5f90a5f5
Andrea Horbach <andrea.horbach@uni-duisburg-essen.de>
2019-02-21 13:35:37 +0100

    Fix test

--- de.unidue.ltl.escrito/de.unidue.ltl.escrito.features/src/test/java/de/unidue/ltl/escrito/features/ngrams/CharNGramsNormalizedFeatureExtractorTest.java
+++ de.unidue.ltl.escrito/de.unidue.ltl.escrito.features/src/test/java/de/unidue/ltl/escrito/features/ngrams/CharNGramsNormalizedFeatureExtractorTest.java
@@ -15,81 +15,82 @@ import de.unidue.ltl.escrito.features.core.io.TestReaderSingleLabelDocumentReade
 
 public class CharNGramsNormalizedFeatureExtractorTest extends LuceneMetaCollectionBasedFeatureTestBase
 {
-	
-	
-	  static String FEATURE_NAME = "23423432434";
-
-	    @Before
-	    public void setup()
-	    {
-	        super.setup();
-	        featureClass = CharNGramsNormalizedFeatureExtractor.class;
-	        metaCollectorClass = CharacterNGramMC.class;
-	    }
-
-	    @Override
-	    protected void evaluateMetaCollection(File luceneFolder) throws Exception
-	    {
-	        Set<String> entriesFromIndex = getEntriesFromIndex(luceneFolder);
-	        System.out.println(entriesFromIndex);
-	        assertEquals(41, entriesFromIndex.size());
-	    }
-
-	    @Override
-	    protected void evaluateExtractedFeatures(File output) throws Exception
-	    {
-	        List<Instance> instances = readInstances(output);
-	        assertEquals(1, instances.size());
 
 
-	        List<Feature> features = new ArrayList<Feature>(instances.get(0).getFeatures());
-	        System.out.println(features);
-	        assertEquals(10, features.size());
-	        assertEquals(features.get(0).getName(), ("CharNGramsNormalizedFeatureExtractor_a"));
-	        assertEquals(features.get(1).getName(), ("CharNGramsNormalizedFeatureExtractor_at"));
-	        assertEquals((double)features.get(0).getValue(), 0.12121212121212122, 0.0001);
-	        assertEquals((double)features.get(1).getValue(), 0.09375, 0.0001);
-		 }
+	static String FEATURE_NAME = "23423432434";
 
-	    @Override
-	    protected CollectionReaderDescription getMetaReader() throws Exception
-	    {
-	        return CollectionReaderFactory.createReaderDescription(TestReaderSingleLabelDocumentReader.class,
-	                TestReaderSingleLabelDocumentReader.PARAM_LANGUAGE, "en",
-	                TestReaderSingleLabelDocumentReader.PARAM_SOURCE_LOCATION,
-	                "src/test/resources/ngrams/text3.txt");
-	    }
+	@Before
+	public void setup()
+	{
+		super.setup();
+		featureClass = CharNGramsNormalizedFeatureExtractor.class;
+		metaCollectorClass = CharacterNGramMC.class;
+	}
 
-	    @Override
-	    protected Object[] getMetaCollectorParameters(File luceneFolder)
-	    {
-	        return new Object[] { CharacterNGramMC.PARAM_UNIQUE_EXTRACTOR_NAME, FEATURE_NAME,
-	                CharNGramsNormalizedFeatureExtractor.PARAM_SOURCE_LOCATION, luceneFolder.toString(),
-	                CharNGramsNormalizedFeatureExtractor.PARAM_NGRAM_USE_TOP_K, 10, 
-	                CharNGramsNormalizedFeatureExtractor.PARAM_NGRAM_MIN_N, 1,
-	                CharNGramsNormalizedFeatureExtractor.PARAM_NGRAM_MAX_N, 2, CharacterNGramMC.PARAM_TARGET_LOCATION,
-	                luceneFolder.toString() };
-	    }
+	@Override
+	protected void evaluateMetaCollection(File luceneFolder) throws Exception
+	{
+		Set<String> entriesFromIndex = getEntriesFromIndex(luceneFolder);
+		System.out.println(entriesFromIndex);
+		assertEquals(43, entriesFromIndex.size());
+	}
 
-	    @Override
-	    protected Object[] getFeatureExtractorParameters(File luceneFolder)
-	    {
-	        return new Object[] { CharNGramsNormalizedFeatureExtractor.PARAM_UNIQUE_EXTRACTOR_NAME, FEATURE_NAME,
-	        		CharNGramsNormalizedFeatureExtractor.PARAM_SOURCE_LOCATION, luceneFolder.toString(),
-	        		CharNGramsNormalizedFeatureExtractor.PARAM_NGRAM_USE_TOP_K, "10", 
-	        		CharNGramsNormalizedFeatureExtractor.PARAM_NGRAM_MIN_N, "1",
-	        		CharNGramsNormalizedFeatureExtractor.PARAM_NGRAM_MAX_N, "2", CharacterNGramMC.PARAM_TARGET_LOCATION,
-	                luceneFolder.toString() };
-	    }
+	@Override
+	protected void evaluateExtractedFeatures(File output) throws Exception
+	{
+		List<Instance> instances = readInstances(output);
+		System.out.println(instances.get(0));
+		assertEquals(1, instances.size());
 
-	    @Override
-	    protected CollectionReaderDescription getFeatureReader() throws Exception
-	    {
-	        return CollectionReaderFactory.createReaderDescription(TestReaderSingleLabelDocumentReader.class,
-	                TestReaderSingleLabelDocumentReader.PARAM_LANGUAGE, "en",
-	                TestReaderSingleLabelDocumentReader.PARAM_SOURCE_LOCATION,
-	                "src/test/resources/ngrams/text3.txt");
-	    }
+
+		List<Feature> features = new ArrayList<Feature>(instances.get(0).getFeatures());
+		System.out.println(features);
+		assertEquals(10, features.size());
+		assertEquals(features.get(0).getName(), ("cNg_normalized_a"));
+		assertEquals(features.get(1).getName(), ("cNg_normalized_c"));
+		assertEquals((double)features.get(0).getValue(), 0.12121212121212122, 0.0001);
+		assertEquals((double)features.get(1).getValue(), 0.12121212121212122, 0.0001);
+	}
+
+	@Override
+	protected CollectionReaderDescription getMetaReader() throws Exception
+	{
+		return CollectionReaderFactory.createReaderDescription(TestReaderSingleLabelDocumentReader.class,
+				TestReaderSingleLabelDocumentReader.PARAM_LANGUAGE, "en",
+				TestReaderSingleLabelDocumentReader.PARAM_SOURCE_LOCATION,
+				"src/test/resources/ngrams/text3.txt");
+	}
+
+	@Override
+	protected Object[] getMetaCollectorParameters(File luceneFolder)
+	{
+		return new Object[] { CharacterNGramMC.PARAM_UNIQUE_EXTRACTOR_NAME, FEATURE_NAME,
+				CharNGramsNormalizedFeatureExtractor.PARAM_SOURCE_LOCATION, luceneFolder.toString(),
+				CharNGramsNormalizedFeatureExtractor.PARAM_NGRAM_USE_TOP_K, 10, 
+				CharNGramsNormalizedFeatureExtractor.PARAM_NGRAM_MIN_N, 1,
+				CharNGramsNormalizedFeatureExtractor.PARAM_NGRAM_MAX_N, 2, CharacterNGramMC.PARAM_TARGET_LOCATION,
+				luceneFolder.toString() };
+	}
+
+	@Override
+	protected Object[] getFeatureExtractorParameters(File luceneFolder)
+	{
+		return new Object[] { CharNGramsNormalizedFeatureExtractor.PARAM_UNIQUE_EXTRACTOR_NAME, FEATURE_NAME,
+				CharNGramsNormalizedFeatureExtractor.PARAM_SOURCE_LOCATION, luceneFolder.toString(),
+				CharNGramsNormalizedFeatureExtractor.PARAM_NGRAM_USE_TOP_K, "10", 
+				CharNGramsNormalizedFeatureExtractor.PARAM_NGRAM_MIN_N, "1",
+				CharNGramsNormalizedFeatureExtractor.PARAM_NGRAM_MAX_N, "2", CharacterNGramMC.PARAM_TARGET_LOCATION,
+				luceneFolder.toString() };
+	}
+
+	@Override
+	protected CollectionReaderDescription getFeatureReader() throws Exception
+	{
+		return CollectionReaderFactory.createReaderDescription(TestReaderSingleLabelDocumentReader.class,
+				TestReaderSingleLabelDocumentReader.PARAM_LANGUAGE, "en",
+				TestReaderSingleLabelDocumentReader.PARAM_SOURCE_LOCATION,
+				"src/test/resources/ngrams/text3.txt");
+	}
 
 
 
