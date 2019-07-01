af3c5fcbbdcff6e1ad3b366a9e17afe17c70edad
Jeanette Bewersdorff <35370899+JeaBew@users.noreply.github.com>
2019-07-01 10:14:45 +0200

    Add files via upload
    uima ruta files

--- de.unidue.ltl.escrito/de.unidue.ltl.escrito.features/src/main/java/de/unidue/ltl/escrito/features/occurence/NrOfRutaPatternMatches.java
+++ de.unidue.ltl.escrito/de.unidue.ltl.escrito.features/src/main/java/de/unidue/ltl/escrito/features/occurence/NrOfRutaPatternMatches.java
@@ -1,5 +1,14 @@
 package de.unidue.ltl.escrito.features.occurence;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.resource.metadata.ConfigurationParameterSettings;
+import org.apache.uima.ruta.engine.RutaEngine;
+import org.apache.uima.util.InvalidXMLException;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Map;
 import java.util.Set;
 
@@ -18,8 +27,16 @@ import org.dkpro.tc.api.type.TextClassificationTarget;
 
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
 
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.cas.CAS;
+import org.apache.uima.cas.Type;
+import org.apache.uima.cas.text.AnnotationFS;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
+import org.apache.uima.fit.util.CasUtil;
+
+
 /**
- * Counts the appearance commas
+ * Counts the appearance of a certain pattern
  */
 @TypeCapability(inputs = { "de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token"})
 public class NrOfRutaPatternMatches extends FeatureExtractorResource_ImplBase
@@ -36,7 +53,8 @@ public class NrOfRutaPatternMatches extends FeatureExtractorResource_ImplBase
     @ConfigurationParameter(name = PARAM_FEATURE_NAME, mandatory = true)
     private String featureName;
 	
-	
+    ArrayList<String> list = new ArrayList<String>();
+    public static String TIER_TYPE = "Tier.Tierart";
 	@Override
 	public boolean initialize(ResourceSpecifier aSpecifier,
 			Map<String,Object> aAdditionalParams) throws ResourceInitializationException {
@@ -44,16 +62,60 @@ public class NrOfRutaPatternMatches extends FeatureExtractorResource_ImplBase
 			return false;
 		}
 		// TODO initialize pattern
+		
+		
 		return true;
 	}
 	
+	//http://www.apache.org/dist/uima/eclipse-update-site/
+	//-> uima ruta installation
 	
 	@Override
 	public Set<Feature> extract(JCas jcas, TextClassificationTarget target) 
 			throws TextClassificationException
 	{
 		// TODO
-		int numberOfMatches = 0;
+		
+		AnalysisEngine engine = null;
+		try {
+			engine = AnalysisEngineFactory.createEngine("uimaRutaScripts.TierEngine");
+		} catch (InvalidXMLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (ResourceInitializationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	
+		CAS cas = null;
+		try {
+			cas = engine.newCAS();
+		} catch (ResourceInitializationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+	
+		
+		ArrayList<String> list = new ArrayList<String>();
+		cas.setDocumentText(jcas.getDocumentText());
+		try {
+			engine.process(cas);
+		} catch (AnalysisEngineProcessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		for (AnnotationFS tier : CasUtil.select(cas, cas.getTypeSystem().getType("Tier.Tierart"))) {
+			list.add(tier.getCoveredText());
+			System.out.println("Found: " + tier.getCoveredText());
+		}
+		System.out.println("Anzahl: " + list.size());
+		
+		int numberOfMatches = list.size();
 		return new Feature(NR_OF_MATCHES+"_"+featureName, numberOfMatches, FeatureType.NUMERIC).asSet();
 	}
 }
