46a853098715268e9acf8533a7205ab5fe6ac47d
Andrea Horbach <andrea.horbach@uni-duisburg-essen.de>
2021-03-15 10:57:35 +0100

    Add Lexical Density FE

---
+++ de.unidue.ltl.escrito/de.unidue.ltl.escrito.features/src/main/java/de/unidue/ltl/escrito/features/complexity/LexicalDensityFeatureExtractor.java
@@ -0,0 +1,63 @@
+package de.unidue.ltl.escrito.features.complexity;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.uima.fit.descriptor.TypeCapability;
+import org.apache.uima.fit.util.JCasUtil;
+import org.apache.uima.jcas.JCas;
+import org.dkpro.tc.api.exception.TextClassificationException;
+import org.dkpro.tc.api.features.Feature;
+import org.dkpro.tc.api.features.FeatureExtractor;
+import org.dkpro.tc.api.features.FeatureExtractorResource_ImplBase;
+import org.dkpro.tc.api.features.FeatureType;
+import org.dkpro.tc.api.type.TextClassificationTarget;
+
+import de.tudarmstadt.ukp.dkpro.core.api.frequency.util.FrequencyDistribution;
+import de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS;
+import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Token;
+
+
+@TypeCapability(inputs = { "de.tudarmstadt.ukp.dkpro.core.api.lexmorph.type.pos.POS" })
+public class LexicalDensityFeatureExtractor
+extends FeatureExtractorResource_ImplBase
+implements FeatureExtractor
+{
+	public static final String FN_LD = "LexicalDensity";
+
+	@Override
+	public Set<Feature> extract(JCas jcas, TextClassificationTarget aTarget)
+			throws TextClassificationException
+	{
+
+		int numberOfContentWords = 0;
+
+		for (POS pos : JCasUtil.select(jcas, POS.class)) {
+	//		System.out.println(pos.getCoarseValue());
+			if (isContentWord(pos.getCoarseValue())){
+				numberOfContentWords++;
+			}
+		}
+	//	System.out.println(numberOfContentWords);
+		double ld = (1.0*numberOfContentWords)/JCasUtil.select(jcas, POS.class).size();
+	//	System.out.println(ld);
+		
+		Set<Feature> features = new HashSet<Feature>();
+		features.add(new Feature(FN_LD, ld, FeatureType.NUMERIC));
+
+		return features;
+	}
+
+	// TODO: Are those all content words? What about adverbs?
+	private boolean isContentWord(String coarseValue) {
+		if (coarseValue.equals("ADJ") || coarseValue.equals("VERB") || coarseValue.startsWith("N")){
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
+
+
---
+++ de.unidue.ltl.escrito/de.unidue.ltl.escrito.features/src/test/java/de/unidue/ltl/escrito/features/complexity/LexicalDensityTest.java
@@ -0,0 +1,52 @@
+package de.unidue.ltl.escrito.features.complexity;
+
+import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
+import static org.dkpro.tc.testing.FeatureTestUtil.assertFeature;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.jcas.JCas;
+import org.dkpro.tc.api.features.Feature;
+import org.dkpro.tc.api.type.TextClassificationTarget;
+import org.junit.Test;
+
+import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
+import de.unidue.ltl.escrito.features.core.EssayGradingTestBase;
+import de.unidue.ltl.escrito.features.core.EssayGradingTestBase.ParserType;
+import de.unidue.ltl.escrito.features.length.NrOfTokens;
+
+import org.junit.Assert;
+
+public class LexicalDensityTest extends EssayGradingTestBase
+{
+    @Test
+    public void lexicalDensityFeatureExtractorTest() 
+        throws Exception
+    {
+    	AnalysisEngine engine = getPreprocessingEngine("de",ParserType.noParser);
+
+
+        JCas jcas = engine.newJCas();
+        jcas.setDocumentLanguage("de");
+        jcas.setDocumentText("Das ist ein Test.");
+        engine.process(jcas);
+
+        TextClassificationTarget target = new TextClassificationTarget(jcas, 0,
+                jcas.getDocumentText().length());
+
+        LexicalDensityFeatureExtractor extractor = new LexicalDensityFeatureExtractor();
+        List<Feature> features = new ArrayList<Feature>(extractor.extract(jcas, target));
+
+        Assert.assertEquals(1, features.size());
+
+        Iterator<Feature> iter = features.iterator();
+        Feature f = iter.next();
+        System.out.println(f.toString());
+        assertFeature(LexicalDensityFeatureExtractor.FN_LD, 5., f);
+    }
+
+    
+}
