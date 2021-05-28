2c998e3f1b48d1ee98a74036de5fa45fe650161c
Andrea Horbach <andrea.horbach@uni-duisburg-essen.de>
2021-05-28 17:39:55 +0200

    Merge branch 'master' of https://github.com/ltl-ude/escrito

--- de.unidue.ltl.escrito/de.unidue.ltl.escrito.features/src/test/java/de/unidue/ltl/escrito/features/core/EssayGradingTestBase.java
+++ de.unidue.ltl.escrito/de.unidue.ltl.escrito.features/src/test/java/de/unidue/ltl/escrito/features/core/EssayGradingTestBase.java
@@ -7,12 +7,14 @@ import org.apache.uima.analysis_engine.AnalysisEngineDescription;
 import org.apache.uima.fit.component.NoOpAnnotator;
 import org.apache.uima.resource.ResourceInitializationException;
 import de.tudarmstadt.ukp.dkpro.core.berkeleyparser.BerkeleyParser;
+import de.tudarmstadt.ukp.dkpro.core.corenlp.CoreNlpDependencyParser;
 import de.tudarmstadt.ukp.dkpro.core.corenlp.CoreNlpLemmatizer;
 import de.tudarmstadt.ukp.dkpro.core.matetools.MateLemmatizer;
 //import de.tudarmstadt.ukp.dkpro.core.matetools.MateLemmatizer;
 //import de.tudarmstadt.ukp.dkpro.core.matetools.MateParser;
 import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpChunker;
 import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpPosTagger;
+import de.tudarmstadt.ukp.dkpro.core.stanfordnlp.StanfordParser;
 import de.tudarmstadt.ukp.dkpro.core.tokit.BreakIteratorSegmenter;
 
 public class EssayGradingTestBase {
@@ -30,51 +32,53 @@ public class EssayGradingTestBase {
 		AnalysisEngineDescription posTagger = createEngineDescription(OpenNlpPosTagger.class);
 		AnalysisEngineDescription chunker = createEngineDescription(OpenNlpChunker.class);
 		AnalysisEngineDescription lemmatizer = createEngineDescription(NoOpAnnotator.class);
+		AnalysisEngineDescription constituentParser = createEngineDescription(NoOpAnnotator.class);
+		AnalysisEngineDescription dependencyParser = createEngineDescription(NoOpAnnotator.class);
 		if (lang.equals("de")){
 			lemmatizer = createEngineDescription(MateLemmatizer.class);
+			dependencyParser = createEngineDescription(CoreNlpDependencyParser.class,
+					CoreNlpDependencyParser.PARAM_LANGUAGE, "de",
+					CoreNlpDependencyParser.PARAM_PRINT_TAGSET,false,
+					CoreNlpDependencyParser.PARAM_VARIANT, "ud");
 		}
 		else if (lang.equals("en")){
 			lemmatizer = createEngineDescription(CoreNlpLemmatizer.class);
+			dependencyParser = createEngineDescription(CoreNlpDependencyParser.class,
+					CoreNlpDependencyParser.PARAM_LANGUAGE, "en",
+					CoreNlpDependencyParser.PARAM_PRINT_TAGSET,false);
 		}
-		//Berkeley Parser		
-		AnalysisEngineDescription berkeleyParserDE = createEngineDescription(BerkeleyParser.class,
-				BerkeleyParser.PARAM_LANGUAGE,"de",BerkeleyParser.PARAM_WRITE_PENN_TREE,true);
-		AnalysisEngineDescription berkeleyparserEN = createEngineDescription(BerkeleyParser.class,
-				BerkeleyParser.PARAM_LANGUAGE,"en",BerkeleyParser.PARAM_WRITE_PENN_TREE,true);
-		//Mate Parser
-		//		AnalysisEngineDescription mateParserDE = createEngineDescription(MateParser.class,
-		//				MateParser.PARAM_LANGUAGE,"de", MateParser.PARAM_PRINT_TAGSET,true
-		//				);
-		//		AnalysisEngineDescription mateParserEN = createEngineDescription(MateParser.class,
-		//				MateParser.PARAM_LANGUAGE,"en", MateParser.PARAM_PRINT_TAGSET,true
-		//				);
-		AnalysisEngineDescription mateParserEN = createEngineDescription(NoOpAnnotator.class);
-		AnalysisEngineDescription mateParserDE = createEngineDescription(NoOpAnnotator.class);
+		constituentParser = createEngineDescription(StanfordParser.class,
+				StanfordParser.PARAM_LANGUAGE, lang,
+				StanfordParser.PARAM_WRITE_PENN_TREE, true,
+				StanfordParser.PARAM_WRITE_POS, false,
+				StanfordParser.PARAM_PRINT_TAGSET,false,
+				StanfordParser.PARAM_VARIANT, "pcfg");
+		
 
 
 		if (lang.equals("de")&&parser.equals(ParserType.noParser)){
 			System.out.println("Build test engine WITHOUT parser for German...");
 			AnalysisEngineDescription description = createEngineDescription(segmenter,posTagger,lemmatizer);
 			return createEngine(description);
-		}else if(lang.equals("de")&&parser.equals(ParserType.constituentParser)){
+		} else if(lang.equals("de")&&parser.equals(ParserType.constituentParser)){
 			System.out.println("Build test engine WITH constituent parser for German...");
-			AnalysisEngineDescription description = createEngineDescription(segmenter,posTagger,lemmatizer,berkeleyParserDE);	
+			AnalysisEngineDescription description = createEngineDescription(segmenter,posTagger,lemmatizer,constituentParser);	
 			return createEngine(description);
-		}else if(lang.equals("de")&&parser.equals(ParserType.dependencyParser)){
+		} else if(lang.equals("de")&&parser.equals(ParserType.dependencyParser)){
 			System.out.println("Build test engine WITH dependency parser for German...");
-			AnalysisEngineDescription description = createEngineDescription(segmenter,posTagger,lemmatizer,mateParserDE);	
+			AnalysisEngineDescription description = createEngineDescription(segmenter,posTagger,lemmatizer,dependencyParser);	
 			return createEngine(description);
-		}else if(lang.equals("en")&&parser.equals(ParserType.noParser)){
+		} else if(lang.equals("en")&&parser.equals(ParserType.noParser)){
 			System.out.println("Build test engine WITHOUT parser for English...");
 			AnalysisEngineDescription description = createEngineDescription(segmenter,posTagger,chunker, lemmatizer);
 			return createEngine(description);
-		}else if(lang.equals("en")&&parser.equals(ParserType.constituentParser)){
+		} else if(lang.equals("en")&&parser.equals(ParserType.constituentParser)){
 			System.out.println("Build test engine WITH parser for English...");
-			AnalysisEngineDescription description = createEngineDescription(segmenter,posTagger,chunker,lemmatizer,berkeleyparserEN);	
+			AnalysisEngineDescription description = createEngineDescription(segmenter,posTagger,chunker,lemmatizer,constituentParser);	
 			return createEngine(description);
 		}else if(lang.equals("en")&&parser.equals(ParserType.dependencyParser)){
 			System.out.println("Build test engine WITH dependency parser for English...");
-			AnalysisEngineDescription description = createEngineDescription(segmenter,posTagger,lemmatizer,mateParserEN);	
+			AnalysisEngineDescription description = createEngineDescription(segmenter,posTagger,lemmatizer,dependencyParser);	
 			return createEngine(description);
 		}else {
 			return null;
