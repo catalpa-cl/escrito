37f2f668a2714a415e46dd72d8d5040ad6447496
Andrea Horbach <andrea.horbach@uni-duisburg-essen.de>
2021-06-17 09:06:55 +0200

    update pom und FeatureSettings

--- de.unidue.ltl.escrito/de.unidue.ltl.escrito.examples/src/main/java/de/unidue/ltl/escrito/examples/basics/FeatureSettings.java
+++ de.unidue.ltl.escrito/de.unidue.ltl.escrito.examples/src/main/java/de/unidue/ltl/escrito/examples/basics/FeatureSettings.java
@@ -28,14 +28,16 @@ import de.unidue.ltl.escrito.features.length.NrOfChars;
 import de.unidue.ltl.escrito.features.length.NrOfSentences;
 import de.unidue.ltl.escrito.features.length.NrOfTokens;
 import de.unidue.ltl.escrito.features.occurence.NrOfCommas;
+import de.unidue.ltl.escrito.features.occurence.NumberOfQuotations;
 import de.unidue.ltl.escrito.features.occurence.NumberOfSubordinateClauses;
+import de.unidue.ltl.escrito.features.occurence.SpeechThoughtWritingRepresentation;
 import de.unidue.ltl.escrito.features.similarity.EmbeddingSimilarityFeatureExtractor;
 import de.unidue.ltl.escrito.features.similarity.PairwiseFeatureWrapper;
 import de.unidue.ltl.escrito.features.similarity.StringSimilarityFeatureExtractor;
 import de.unidue.ltl.escrito.features.similarity.WordOverlapFeatureExtractor;
 
 public class FeatureSettings
-	implements Constants
+implements Constants
 {
 
 
@@ -52,18 +54,36 @@ public class FeatureSettings
 								WordNGram.class,
 								WordNGram.PARAM_NGRAM_MIN_N, 1,
 								WordNGram.PARAM_NGRAM_MAX_N, 3,
-								WordNGram.PARAM_NGRAM_USE_TOP_K, 1000
+								WordNGram.PARAM_NGRAM_USE_TOP_K, 10000
 								),
 						TcFeatureFactory.create(
 								CharacterNGram.class,
 								CharacterNGram.PARAM_NGRAM_MIN_N, 2,
 								CharacterNGram.PARAM_NGRAM_MAX_N, 5,
-								CharacterNGram.PARAM_NGRAM_USE_TOP_K, 1000
+								CharacterNGram.PARAM_NGRAM_USE_TOP_K, 10000
 								)
 						)
 				);
 		return dimFeatureSets;
 	}
+
+
+	public static Dimension<TcFeatureSet> getFeatureSetNGrams()
+	{
+		Dimension<TcFeatureSet> dimFeatureSets = Dimension.create(
+				DIM_FEATURE_SET,
+				new TcFeatureSet(
+						//			TcFeatureFactory.create(NrOfTokens.class),
+						TcFeatureFactory.create(
+								WordNGram.class,
+								WordNGram.PARAM_NGRAM_MIN_N, 1,
+								WordNGram.PARAM_NGRAM_MAX_N, 3,
+								WordNGram.PARAM_NGRAM_USE_TOP_K, 10000
+								)
+						)
+				);
+		return dimFeatureSets;
+	}		
 
 
 	/*
@@ -123,7 +143,7 @@ public class FeatureSettings
 		return dimFeatureSets;
 	}
 
-	
+
 
 	public static Dimension<TcFeatureSet> getFeatureSetsEssayFull()
 	{
@@ -148,6 +168,12 @@ public class FeatureSettings
 								SkipWordNGram.PARAM_NGRAM_MIN_N, 2,
 								SkipWordNGram.PARAM_NGRAM_MAX_N, 5,
 								SkipWordNGram.PARAM_NGRAM_USE_TOP_K, 10000
+								),					
+						TcFeatureFactory.create(
+								CharacterNGram.class,
+								CharacterNGram.PARAM_NGRAM_MIN_N, 2,
+								CharacterNGram.PARAM_NGRAM_MAX_N, 5,
+								CharacterNGram.PARAM_NGRAM_USE_TOP_K, 10000
 								),
 						// Length
 						TcFeatureFactory.create(NrOfTokens.class),
@@ -166,14 +192,15 @@ public class FeatureSettings
 						TcFeatureFactory.create(NrOfCommas.class),
 						TcFeatureFactory.create(
 								NrOfConnectives.class,
-								NrOfConnectives.PARAM_CONNECTIVES_FILE_PATH, "src/main/resources/lists/de/connectives_de.txt"
+								NrOfConnectives.PARAM_CONNECTIVES_FILE_PATH, "src/main/resources/lists/en/connectives_en.txt"
 								),
-//						TcFeatureFactory.create(NumberOfQuotations.class),
-//						TcFeatureFactory.create(
-//								SpeechThoughtWritingRepresentation.class,
-//								SpeechThoughtWritingRepresentation.PARAM_REPORTING_VERBS_FILE_PATH, "src/main/resources/lists/de/reporting_verbs_krestel_en.txt"
-//								),
-
+						TcFeatureFactory.create(NumberOfQuotations.class),
+						TcFeatureFactory.create(
+								SpeechThoughtWritingRepresentation.class,
+								SpeechThoughtWritingRepresentation.PARAM_LANGUAGE, "en",
+								//SpeechThoughtWritingRepresentation.PARAM_REPORTING_VERBS_FILE_PATH, "src/main/resources/lists/de/reporting_verbs_krestel_de.txt"
+								SpeechThoughtWritingRepresentation.PARAM_REPORTING_VERBS_FILE_PATH, "src/main/resources/lists/en/reporting_verbs_krestel_en.txt"
+								),
 						TcFeatureFactory.create(TypeTokenRatioFeatureExtractor.class),
 						TcFeatureFactory.create(
 								PairwiseSentenceSimilarity.class,
@@ -183,43 +210,69 @@ public class FeatureSettings
 						TcFeatureFactory.create(
 								NumberOfSubordinateClauses.class,
 								NumberOfSubordinateClauses.PARAM_LANGUAGE, "de",
-								NumberOfSubordinateClauses.PARAM_CAUSAL_INDICATORS_FILE_PATH, "src/main/resources/lists/de/causalIndicators_de.txt",
-								NumberOfSubordinateClauses.PARAM_TEMPORAL_INDICATORS_FILE_PATH, "src/main/resources/lists/de/temporalIndicators_de.txt"
+								//								NumberOfSubordinateClauses.PARAM_CAUSAL_INDICATORS_FILE_PATH, "src/main/resources/lists/de/causalIndicators_de.txt",
+								//								NumberOfSubordinateClauses.PARAM_TEMPORAL_INDICATORS_FILE_PATH, "src/main/resources/lists/de/temporalIndicators_de.txt"
+								NumberOfSubordinateClauses.PARAM_CAUSAL_INDICATORS_FILE_PATH, "src/main/resources/lists/en/causalIndicators_en.txt",
+								NumberOfSubordinateClauses.PARAM_TEMPORAL_INDICATORS_FILE_PATH, "src/main/resources/lists/en/temporalIndicators_en.txt"
 								),
 						TcFeatureFactory.create(SyntacticVariability.class),
-						TcFeatureFactory.create(TraditionalReadabilityMeasures.class),
-						TcFeatureFactory.create(NumberOfGrammarMistakes.class,
-								NumberOfGrammarMistakes.PARAM_INPUT_LOCATION, "src/main/resources/lists/de/grammarMistakes.txt")
+						TcFeatureFactory.create(TraditionalReadabilityMeasures.class)
+						//						TcFeatureFactory.create(NumberOfGrammarMistakes.class,
+						//								NumberOfGrammarMistakes.PARAM_INPUT_LOCATION, "src/main/resources/lists/de/grammarMistakes.txt")
 						)
 				);
 		return dimFeatureSets;
 	}
 
 
-	public static Dimension<?> getFeatureSetCrowdfest(String corpusName) {
+	public static Dimension<TcFeatureSet> getFeatureSetsEssayNoNgrams()
+	{
 		Dimension<TcFeatureSet> dimFeatureSets = Dimension.create(
 				DIM_FEATURE_SET,
 				new TcFeatureSet(
+						TcFeatureFactory.create(NrOfTokens.class),
+						TcFeatureFactory.create(NrOfChars.class),
+						TcFeatureFactory.create(NrOfSentences.class),
+						TcFeatureFactory.create(AvgNrOfTokensPerSentence.class),
+						TcFeatureFactory.create(AvgNrOfCharsPerSentence.class),
+						TcFeatureFactory.create(AvgNrOfCharsPerToken.class),
+						TcFeatureFactory.create(TokenLengthRatio.class),
+
+						TcFeatureFactory.create(POSRatioFeatureExtractor.class),
+						TcFeatureFactory.create(PronounRatioFeatureExtractor.class),
+						TcFeatureFactory.create(QuestionsRatioFeatureExtractor.class),
+						TcFeatureFactory.create(AdjectiveEndingFeatureExtractor.class),
+
+						TcFeatureFactory.create(NrOfCommas.class),
 						TcFeatureFactory.create(
-								NrOfTokens.class
+								NrOfConnectives.class,
+								NrOfConnectives.PARAM_CONNECTIVES_FILE_PATH, "src/main/resources/lists/en/connectives_en.txt"
 								),
+						TcFeatureFactory.create(NumberOfQuotations.class),
 						TcFeatureFactory.create(
-								NrOfChars.class
+								SpeechThoughtWritingRepresentation.class,
+								SpeechThoughtWritingRepresentation.PARAM_LANGUAGE, "en",
+								//SpeechThoughtWritingRepresentation.PARAM_REPORTING_VERBS_FILE_PATH, "src/main/resources/lists/de/reporting_verbs_krestel_de.txt"
+								SpeechThoughtWritingRepresentation.PARAM_REPORTING_VERBS_FILE_PATH, "src/main/resources/lists/en/reporting_verbs_krestel_en.txt"
 								),
+						TcFeatureFactory.create(TypeTokenRatioFeatureExtractor.class),
 						TcFeatureFactory.create(
-								PairwiseFeatureWrapper.class,
-								PairwiseFeatureWrapper.PARAM_PAIRWISE_FEATURE_EXTRACTOR, WordOverlapFeatureExtractor.class.getName(),
-								PairwiseFeatureWrapper.PARAM_TARGET_ANSWER_PREFIX, "TA",
-								PairwiseFeatureWrapper.PARAM_AGGREGATION_METHOD, PairwiseFeatureWrapper.AggregationMethod.MAXIMUM,
-								PairwiseFeatureWrapper.PARAM_ADDITIONAL_TEXTS_LOCATION, System.getenv("DKPRO_HOME")+"/processedData/"+corpusName
+								PairwiseSentenceSimilarity.class,
+								PairwiseSentenceSimilarity.PARAM_NR_OF_SAMPLES, 5
 								),
+						TcFeatureFactory.create(SyntaxTreeDepth.class, SyntaxTreeDepth.LANGUAGE, "de"),
 						TcFeatureFactory.create(
-								PairwiseFeatureWrapper.class,
-								PairwiseFeatureWrapper.PARAM_PAIRWISE_FEATURE_EXTRACTOR, StringSimilarityFeatureExtractor.class.getName(),
-								PairwiseFeatureWrapper.PARAM_TARGET_ANSWER_PREFIX, "TA",
-								PairwiseFeatureWrapper.PARAM_AGGREGATION_METHOD, PairwiseFeatureWrapper.AggregationMethod.MAXIMUM,
-								PairwiseFeatureWrapper.PARAM_ADDITIONAL_TEXTS_LOCATION, System.getenv("DKPRO_HOME")+"/processedData/"+corpusName
-								)
+								NumberOfSubordinateClauses.class,
+								NumberOfSubordinateClauses.PARAM_LANGUAGE, "de",
+								//								NumberOfSubordinateClauses.PARAM_CAUSAL_INDICATORS_FILE_PATH, "src/main/resources/lists/de/causalIndicators_de.txt",
+								//								NumberOfSubordinateClauses.PARAM_TEMPORAL_INDICATORS_FILE_PATH, "src/main/resources/lists/de/temporalIndicators_de.txt"
+								NumberOfSubordinateClauses.PARAM_CAUSAL_INDICATORS_FILE_PATH, "src/main/resources/lists/en/causalIndicators_en.txt",
+								NumberOfSubordinateClauses.PARAM_TEMPORAL_INDICATORS_FILE_PATH, "src/main/resources/lists/en/temporalIndicators_en.txt"
+								),
+						TcFeatureFactory.create(SyntacticVariability.class),
+						TcFeatureFactory.create(TraditionalReadabilityMeasures.class)
+						//						TcFeatureFactory.create(NumberOfGrammarMistakes.class,
+						//								NumberOfGrammarMistakes.PARAM_INPUT_LOCATION, "src/main/resources/lists/de/grammarMistakes.txt")
 						)
 				);
 		return dimFeatureSets;
